7b7ecbd580e89a71e0abdf3f55d1a30fe2499957
zuzannawozniak <zuzanna.wk@interia.pl>
2023-12-01 13:22:31 +0100

    add: Worker

---
+++ Backend/CarService/src/main/java/com/carservice/CarService/worker/Worker.java
@@ -0,0 +1,9 @@
+package com.carservice.CarService.worker;
+
+import com.carservice.CarService.personalData.PersonalData;
+
+import java.math.BigDecimal;
+
+public abstract class Worker extends PersonalData {
+    private BigDecimal salaryPerHour;
+}
